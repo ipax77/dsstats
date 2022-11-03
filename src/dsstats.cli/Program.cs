bd8a6cfeece050a1894ffd321aa2942caa117d9f
ipax77 <ph.ilipp@web.de>
2022-11-03 12:50:01 +0100

    sc2dsstats.cli decode

--- src/dsstats.cli/Program.cs
+++ src/dsstats.cli/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System.Reflection;
+using System.Text.Json;
+using pax.dsstats.parser;
+using pax.dsstats.shared;
+using s2protocol.NET;
 
 namespace dsstats.cli;
 class Program
 {
-    static void Main(string[] args)
+    public static readonly string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "";
+    private static readonly CancellationTokenSource cts = new();
+
+    static async Task Main(string[] args)
     {
-        if (args.Length == 0)
+        if (args.Length < 3)
         {
+            WriteHowToUse();
+            return;
+        }
+
+        if (args[0] == "decode")
+        {
+            if (!Directory.Exists(args[1]) || !Directory.Exists(args[2]))
+            {
+                Console.WriteLine($"Directory not found :(");
+            }
+            await Decode(args[1], args[2]);
+        }
+        else
+        {
+            WriteHowToUse();
+            return;
+        }
+    }
+
+    private static void WriteHowToUse()
+    {
             var versionString = Assembly.GetEntryAssembly()?
                                     .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
                                     .InformationalVersion
@@ -15,8 +43,57 @@ class Program
             Console.WriteLine($"dsstats.cli v{versionString}");
             Console.WriteLine("-------------");
             Console.WriteLine("\nUsage:");
-            Console.WriteLine("  unpack <filename>");
-            // return;
+            Console.WriteLine("  decode <replayPath> <outputPath>");
+    }
+
+    private static async Task Decode(string replaysPath, string outputPath)
+    {
+        ReplayDecoder decoder = new(assemblyPath);
+
+        ReplayDecoderOptions decoderOptions = new()
+        {
+            Initdata = true,
+            Details = true,
+            Metadata = true,
+            MessageEvents = false,
+            TrackerEvents = true,
+            GameEvents = false,
+            AttributeEvents = false
+        };
+
+        var replayPaths = Directory.GetFiles(replaysPath, "*.SC2Replay", SearchOption.TopDirectoryOnly);
+
+        await foreach (var sc2rep in decoder.DecodeParallel(replayPaths, 4, decoderOptions, cts.Token))
+        {
+            if (cts.IsCancellationRequested)
+            {
+                break;
+            }
+
+            try
+            {
+                var dsRep = Parse.GetDsReplay(sc2rep);
+                if (dsRep != null)
+                {
+                    var dtoRep = Parse.GetReplayDto(dsRep);
+                    SaveReplay(dtoRep, outputPath);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"failed decoding replay {sc2rep?.FileName}: {ex.Message}");
+            }
         }
+    }
+
+    private static void SaveReplay(ReplayDto? replayDto, string outputPath)
+    {
+        if (replayDto == null || String.IsNullOrEmpty(replayDto.FileName))
+        {
+            return;
+        }
+        var json = JsonSerializer.Serialize(replayDto);
+        var outputFileName = Path.Combine(outputPath, Path.GetFileNameWithoutExtension(replayDto.FileName) + ".json");
+        File.WriteAllText(outputFileName, json);
     }
 }
