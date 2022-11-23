fd77753fa3bbd8f012dd1c0d84d23da9898de5e6
ipax77 <ph.ilipp@web.de>
2022-11-23 06:37:22 +0100

    Rating minGames >= 20

--- src/pax.dsstats.web/dsstats.raven/RatingRepository.Memory.cs
+++ src/pax.dsstats.web/dsstats.raven/RatingRepository.Memory.cs
@@ -79,13 +79,13 @@ public partial class RatingRepository
         if (request.Type == RatingType.Cmdr)
         {
             ratings = RatingMemory.Values
-                .Where(x => x.CmdrPlayer != null)
+                .Where(x => x.CmdrPlayer != null && x.CmdrPlayer.Rating.Games >= 20)
                 .Select(s => s.CmdrPlayer ?? new()).AsQueryable();
         }
         else if (request.Type == RatingType.Std)
         {
             ratings = RatingMemory.Values
-                .Where(x => x.StdPlayer != null)
+                .Where(x => x.StdPlayer != null && x.StdPlayer.Rating.Games >= 20)
                 .Select(s => s.StdPlayer ?? new()).AsQueryable();
         }
         else
