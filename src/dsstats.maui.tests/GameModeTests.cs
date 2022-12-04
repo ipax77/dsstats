09afe7ced6f6e616b3785aeae82b4a52526b4c38
ipax77 <ph.ilipp@web.de>
2022-12-04 16:41:25 +0100

    Fix Standard 1v1 GameMode

--- src/dsstats.maui.tests/GameModeTests.cs
+++ src/dsstats.maui.tests/GameModeTests.cs
@@ -9,9 +9,10 @@ public class GameModeTests
     public static readonly string? assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
     [Theory]
+    [InlineData("C:\\Users\\pax77\\Documents\\StarCraft II\\Accounts\\107095918\\2-S2-1-226401\\Replays\\Multiplayer\\Direct Strike (5190).SC2Replay")]
     [InlineData("C:\\Users\\pax77\\Documents\\StarCraft II\\Accounts\\107095918\\2-S2-1-226401\\Replays\\Multiplayer\\Direct Strike (3553).SC2Replay")]
     [InlineData("C:\\Users\\pax77\\Documents\\StarCraft II\\Accounts\\107095918\\2-S2-1-226401\\Replays\\Multiplayer\\Direct Strike (1370).SC2Replay")]
-    public async Task ParseReplayTest(string replayFile)
+    public async Task GameModeStandardTest(string replayFile)
     {
         Assert.True(assemblyPath != null, "Could not get ExecutingAssembly path");
         if (assemblyPath == null)
@@ -48,20 +49,261 @@ public class GameModeTests
 
         var replayDto = Parse.GetReplayDto(dsReplay);
 
-        var replayPlayerMaxDuration = replayDto.ReplayPlayers.Select(s => s.Duration).Max();
-        var uploader = replayDto.ReplayPlayers.FirstOrDefault(f => f.Duration == replayPlayerMaxDuration);
+        Assert.True(replayDto.GameMode == pax.dsstats.shared.GameMode.Standard);
+    }
 
-        Assert.NotNull(uploader);
-        if (uploader == null)
+    [Theory]
+    [InlineData("C:\\Users\\pax77\\Documents\\StarCraft II\\Accounts\\107095918\\2-S2-1-226401\\Replays\\Multiplayer\\Direct Strike (763).SC2Replay")]
+    [InlineData("C:\\Users\\pax77\\Documents\\StarCraft II\\Accounts\\107095918\\2-S2-1-226401\\Replays\\Multiplayer\\Direct Strike (32).SC2Replay")]
+    public async Task GameModeHeroicTest(string replayFile)
+    {
+        Assert.True(assemblyPath != null, "Could not get ExecutingAssembly path");
+        if (assemblyPath == null)
+        {
+            return;
+        }
+        ReplayDecoder decoder = new(assemblyPath);
+        ReplayDecoderOptions options = new ReplayDecoderOptions()
+        {
+            Initdata = true,
+            Details = true,
+            Metadata = true,
+            MessageEvents = false,
+            TrackerEvents = true,
+            GameEvents = false,
+            AttributeEvents = false,
+        };
+        var replay = await decoder.DecodeAsync(Path.Combine(assemblyPath, "testdata", replayFile), options).ConfigureAwait(false);
+        Assert.True(replay != null, "Sc2Replay was null");
+        if (replay == null)
         {
             decoder.Dispose();
             return;
         }
 
-        foreach (var replayPlayer in replayDto.ReplayPlayers.Where(x => x.Spawns.Any()))
+        var dsReplay = Parse.GetDsReplay(replay);
+
+        Assert.NotNull(dsReplay);
+        if (dsReplay == null)
         {
-            Assert.NotNull(replayPlayer.Spawns.FirstOrDefault(f => f.Breakpoint == pax.dsstats.shared.Breakpoint.All));
+            decoder.Dispose();
+            return;
         }
-        decoder.Dispose();
+
+        var replayDto = Parse.GetReplayDto(dsReplay);
+
+        Assert.True(replayDto.GameMode == pax.dsstats.shared.GameMode.CommandersHeroic);
+    }
+
+    [Theory]
+    [InlineData("C:\\Users\\pax77\\Documents\\StarCraft II\\Accounts\\107095918\\2-S2-1-226401\\Replays\\Multiplayer\\Direct Strike (5057).SC2Replay")]
+    [InlineData("C:\\Users\\pax77\\Documents\\StarCraft II\\Accounts\\107095918\\2-S2-1-226401\\Replays\\Multiplayer\\Direct Strike (1782).SC2Replay")]
+    public async Task GameModeBrawlCmdrTest(string replayFile)
+    {
+        Assert.True(assemblyPath != null, "Could not get ExecutingAssembly path");
+        if (assemblyPath == null)
+        {
+            return;
+        }
+        ReplayDecoder decoder = new(assemblyPath);
+        ReplayDecoderOptions options = new ReplayDecoderOptions()
+        {
+            Initdata = true,
+            Details = true,
+            Metadata = true,
+            MessageEvents = false,
+            TrackerEvents = true,
+            GameEvents = false,
+            AttributeEvents = false,
+        };
+        var replay = await decoder.DecodeAsync(Path.Combine(assemblyPath, "testdata", replayFile), options).ConfigureAwait(false);
+        Assert.True(replay != null, "Sc2Replay was null");
+        if (replay == null)
+        {
+            decoder.Dispose();
+            return;
+        }
+
+        var dsReplay = Parse.GetDsReplay(replay);
+
+        Assert.NotNull(dsReplay);
+        if (dsReplay == null)
+        {
+            decoder.Dispose();
+            return;
+        }
+
+        var replayDto = Parse.GetReplayDto(dsReplay);
+
+        Assert.True(replayDto.GameMode == pax.dsstats.shared.GameMode.BrawlCommanders);
+    }
+
+    [Theory]
+    [InlineData("C:\\Users\\pax77\\Documents\\StarCraft II\\Accounts\\107095918\\2-S2-1-226401\\Replays\\Multiplayer\\Direct Strike (1312).SC2Replay")]
+    [InlineData("C:\\Users\\pax77\\Documents\\StarCraft II\\Accounts\\107095918\\2-S2-1-226401\\Replays\\Multiplayer\\Direct Strike (4547).SC2Replay")]
+    public async Task GameModeBrawlStdTest(string replayFile)
+    {
+        Assert.True(assemblyPath != null, "Could not get ExecutingAssembly path");
+        if (assemblyPath == null)
+        {
+            return;
+        }
+        ReplayDecoder decoder = new(assemblyPath);
+        ReplayDecoderOptions options = new ReplayDecoderOptions()
+        {
+            Initdata = true,
+            Details = true,
+            Metadata = true,
+            MessageEvents = false,
+            TrackerEvents = true,
+            GameEvents = false,
+            AttributeEvents = false,
+        };
+        var replay = await decoder.DecodeAsync(Path.Combine(assemblyPath, "testdata", replayFile), options).ConfigureAwait(false);
+        Assert.True(replay != null, "Sc2Replay was null");
+        if (replay == null)
+        {
+            decoder.Dispose();
+            return;
+        }
+
+        var dsReplay = Parse.GetDsReplay(replay);
+
+        Assert.NotNull(dsReplay);
+        if (dsReplay == null)
+        {
+            decoder.Dispose();
+            return;
+        }
+
+        var replayDto = Parse.GetReplayDto(dsReplay);
+
+        Assert.True(replayDto.GameMode == pax.dsstats.shared.GameMode.BrawlStandard);
+    }
+
+    [Theory]
+    [InlineData("C:\\Users\\pax77\\Documents\\StarCraft II\\Accounts\\107095918\\2-S2-1-226401\\Replays\\Multiplayer\\Direct Strike (5191).SC2Replay")]
+    public async Task GameModeGearTest(string replayFile)
+    {
+        Assert.True(assemblyPath != null, "Could not get ExecutingAssembly path");
+        if (assemblyPath == null)
+        {
+            return;
+        }
+        ReplayDecoder decoder = new(assemblyPath);
+        ReplayDecoderOptions options = new ReplayDecoderOptions()
+        {
+            Initdata = true,
+            Details = true,
+            Metadata = true,
+            MessageEvents = false,
+            TrackerEvents = true,
+            GameEvents = false,
+            AttributeEvents = false,
+        };
+        var replay = await decoder.DecodeAsync(Path.Combine(assemblyPath, "testdata", replayFile), options).ConfigureAwait(false);
+        Assert.True(replay != null, "Sc2Replay was null");
+        if (replay == null)
+        {
+            decoder.Dispose();
+            return;
+        }
+
+        var dsReplay = Parse.GetDsReplay(replay);
+
+        Assert.NotNull(dsReplay);
+        if (dsReplay == null)
+        {
+            decoder.Dispose();
+            return;
+        }
+
+        var replayDto = Parse.GetReplayDto(dsReplay);
+
+        Assert.True(replayDto.GameMode == pax.dsstats.shared.GameMode.Gear);
+    }
+
+    [Theory]
+    [InlineData("C:\\Users\\pax77\\Documents\\StarCraft II\\Accounts\\107095918\\2-S2-1-226401\\Replays\\Multiplayer\\Direct Strike (5192).SC2Replay")]
+    public async Task GameModeSwitchTest(string replayFile)
+    {
+        Assert.True(assemblyPath != null, "Could not get ExecutingAssembly path");
+        if (assemblyPath == null)
+        {
+            return;
+        }
+        ReplayDecoder decoder = new(assemblyPath);
+        ReplayDecoderOptions options = new ReplayDecoderOptions()
+        {
+            Initdata = true,
+            Details = true,
+            Metadata = true,
+            MessageEvents = false,
+            TrackerEvents = true,
+            GameEvents = false,
+            AttributeEvents = false,
+        };
+        var replay = await decoder.DecodeAsync(Path.Combine(assemblyPath, "testdata", replayFile), options).ConfigureAwait(false);
+        Assert.True(replay != null, "Sc2Replay was null");
+        if (replay == null)
+        {
+            decoder.Dispose();
+            return;
+        }
+
+        var dsReplay = Parse.GetDsReplay(replay);
+
+        Assert.NotNull(dsReplay);
+        if (dsReplay == null)
+        {
+            decoder.Dispose();
+            return;
+        }
+
+        var replayDto = Parse.GetReplayDto(dsReplay);
+
+        Assert.True(replayDto.GameMode == pax.dsstats.shared.GameMode.Switch);
+    }
+
+    [Theory]
+    [InlineData("C:\\Users\\pax77\\Documents\\StarCraft II\\Accounts\\107095918\\2-S2-1-226401\\Replays\\Multiplayer\\Direct Strike (5193).SC2Replay")]
+    public async Task GameModeSabotageTest(string replayFile)
+    {
+        Assert.True(assemblyPath != null, "Could not get ExecutingAssembly path");
+        if (assemblyPath == null)
+        {
+            return;
+        }
+        ReplayDecoder decoder = new(assemblyPath);
+        ReplayDecoderOptions options = new ReplayDecoderOptions()
+        {
+            Initdata = true,
+            Details = true,
+            Metadata = true,
+            MessageEvents = false,
+            TrackerEvents = true,
+            GameEvents = false,
+            AttributeEvents = false,
+        };
+        var replay = await decoder.DecodeAsync(Path.Combine(assemblyPath, "testdata", replayFile), options).ConfigureAwait(false);
+        Assert.True(replay != null, "Sc2Replay was null");
+        if (replay == null)
+        {
+            decoder.Dispose();
+            return;
+        }
+
+        var dsReplay = Parse.GetDsReplay(replay);
+
+        Assert.NotNull(dsReplay);
+        if (dsReplay == null)
+        {
+            decoder.Dispose();
+            return;
+        }
+
+        var replayDto = Parse.GetReplayDto(dsReplay);
+
+        Assert.True(replayDto.GameMode == pax.dsstats.shared.GameMode.Sabotage);
     }
 }
