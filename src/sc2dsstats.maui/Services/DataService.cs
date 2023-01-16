f5665901e3f765d792933748d00b1f225345b793
ipax77 <ph.ilipp@web.de>
2023-01-16 07:29:47 +0100

    cu

--- src/sc2dsstats.maui/Services/DataService.cs
+++ src/sc2dsstats.maui/Services/DataService.cs
@@ -29,8 +29,8 @@ public partial class DataService : IDataService
         this.fromServerSwitchService = fromServerSwitchService;
         this.logger = logger;
         httpClient = new HttpClient();
-        httpClient.BaseAddress = new Uri("https://localhost:7174");
-        // httpClient.BaseAddress = new Uri("https://dsstats.pax77.org");
+        // httpClient.BaseAddress = new Uri("https://localhost:7174");
+        httpClient.BaseAddress = new Uri("https://dsstats.pax77.org");
         httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
     }
 
