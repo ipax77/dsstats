99f092274d0a41b19c0f1318cad3f86a0d01064c
ipax77 <ph.ilipp@web.de>
2023-07-28 12:26:33 +0200

    TeamComp with AvgGain

--- src/pax.dsstats.shared/Crosstable/CrossTableRequest.cs
+++ src/pax.dsstats.shared/Crosstable/CrossTableRequest.cs
@@ -15,3 +15,34 @@ public record CrossTableReplaysRequest
     public TeamCmdrs TeamCmdrs { get; set; } = null!;
     public TeamCmdrs? TeamCmdrsVs { get; set; }
 }
+
+public record TeamCompRequest
+{
+    public TimePeriod TimePeriod { get; set; }
+    public RatingType RatingType { get; set; }
+    public bool WithLeavers { get; set; }
+    public string? Interest { get; set; }
+}
+
+public record TeamCompResponse
+{
+    public string? Team { get; set; }
+    public List<TeamResponseItem> Items { get; set; } = new();
+    public List<TeamReplayInfo> Replays { get; set; } = new();
+}
+
+public record TeamResponseItem
+{
+    public string Team { get; set; } = string.Empty;
+    public int Count { get; set; }
+    public int Wins { get; set; }
+    public double AvgGain { get; set; }
+}
+
+public record TeamReplayInfo
+{
+    public DateTime GameTime { get; set; }
+    public string ReplayHash { get; set; } = string.Empty;
+    public string CommandersTeam1 { get; set; } = string.Empty;
+    public string CommandersTeam2 { get; set; } = string.Empty;
+}
---
+++ src/pax.dsstats.web/Client/Services/TeamService.cs
@@ -0,0 +1,41 @@
+﻿using pax.dsstats.shared;
+using System.Net.Http.Json;
+
+namespace pax.dsstats.web.Client.Services;
+
+public class TeamService : ITeamService
+{
+    private readonly HttpClient httpClient;
+    private readonly ILogger<TeamService> logger;
+    private readonly string statsController = "api/v6/Stats";
+
+    public TeamService(HttpClient httpClient, ILogger<TeamService> logger)
+    {
+        this.httpClient = httpClient;
+        this.logger = logger;
+    }
+
+    public async Task<TeamCompResponse> GetTeamRating(TeamCompRequest request, CancellationToken token)
+    {
+        try
+        {
+            var response = await httpClient.PostAsJsonAsync($"{statsController}/teamcomp", request, token);
+            response.EnsureSuccessStatusCode();
+            var data = await response.Content.ReadFromJsonAsync<TeamCompResponse>();
+
+            if (data == null)
+            {
+                logger.LogError($"failed getting teamcomp");
+            }
+            else
+            {
+                return data;
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError($"failed getting teamcomp: {ex.Message}");
+        }
+        return new();
+    }
+}
