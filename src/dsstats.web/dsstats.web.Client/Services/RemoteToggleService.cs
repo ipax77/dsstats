3e4951292023587ce2ebc8f49be0f0792a6b2560
ipax77 <ph.ilipp@web.de>
2023-12-07 23:09:04 +0100

    new replays scan refactored

--- src/dsstats.web/dsstats.web.Client/Services/RemoteToggleService.cs
+++ src/dsstats.web/dsstats.web.Client/Services/RemoteToggleService.cs
@@ -4,13 +4,26 @@ namespace dsstats.web.Client.Services;
 
 public class RemoteToggleService : IRemoteToggleService
 {
+    private string _culture = "en";
     public bool FromServer => true;
 
     public bool IsMaui => false;
 
-#pragma warning disable CS0414 // dummy for server side, only useful for maui
+    public string Culture => _culture;
+
     public event EventHandler? FromServerChanged = null;
-#pragma warning restore CS0414
+    public event EventHandler? CultureChanged;
+
+    protected void OnCultureChanged()
+    {
+        CultureChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void SetCulture(string culture)
+    {
+        _culture = culture;
+        OnCultureChanged();
+    }
 
     public void SetFromServer(bool fromServer)
     {
