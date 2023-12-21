da2b13ef6b6f9a8f8ec3c24569114f594e641e3e
ipax77 <ph.ilipp@web.de>
2023-12-21 21:40:55 +0100

    cu

--- src/dsstats.razorlib/wwwroot/js/annotationChart.js
+++ src/dsstats.razorlib/wwwroot/js/annotationChart.js
@@ -5,53 +5,6 @@ export async function registerPlugin() {
     Chart.register(annotationPlugin);
 }
 
-export function drawArrows(team, chartId) {
-    const chart = Chart.getChart(chartId);
-
-    if (chart == undefined) {
-        return;
-    }
-
-    const annotations = {
-        annotations: {
-            pentagon1: {
-                type: 'polygon',
-                xValue: 1,
-                yValue: 60,
-                sides: 5,
-                radius: 60,
-                backgroundColor: 'rgba(255, 99, 132, 0.25)'
-            },
-            pentagon2: {
-                type: 'polygon',
-                xValue: 4,
-                yValue: 60,
-                sides: 5,
-                radius: 60,
-                backgroundColor: 'rgba(255, 99, 132, 0.25)'
-            }
-        }
-    };
-
-
-    //if (team == 1) {
-    //    annotations.annotations.pentagon1.yMin = 1;
-    //    annotations.annotations.pentagon1.yMax = 5;
-    //    annotations.annotations.pentagon2.yMin = 1;
-    //    annotations.annotations.pentagon2.yMax = 5;
-
-    //} else {
-    //    annotations.annotations.pentagon1.yMin = 11;
-    //    annotations.annotations.pentagon1.yMax = 15;
-    //    annotations.annotations.pentagon2.yMin = 11;
-    //    annotations.annotations.pentagon2.yMax = 15;
-    //}
-
-    chart.config.options.plugins["annotation"] = annotations;
-
-    chart.update();
-}
-
 export function setChartTooltips(chartId) {
     const chart = Chart.getChart(chartId);
 
@@ -63,7 +16,7 @@ export function setChartTooltips(chartId) {
         if (tooltipItem == undefined) {
             return "";
         } else {
-            return tooltipItem.raw.label + " " + tooltipItem.raw.x + "|" + tooltipItem.raw.y;
+            return tooltipItem.raw.label;
         }
     };
 }
