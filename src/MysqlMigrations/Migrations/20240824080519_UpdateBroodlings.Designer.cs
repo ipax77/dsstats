206a1d915eaa6ec6725f033fc8ea7031bde89bb2
ipax77 <ph.ilipp@web.de>
2024-08-24 10:18:45 +0200

    ChartJs v4.4.4, pax.BlazorChartJs v8.0.4

--- src/MysqlMigrations/Migrations/20240824080519_UpdateBroodlings.Designer.cs
+++ src/MysqlMigrations/Migrations/20240824080519_UpdateBroodlings.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using dsstats.db8;
 
@@ -11,9 +12,11 @@ using dsstats.db8;
 namespace MysqlMigrations.Migrations
 {
     [DbContext(typeof(ReplayContext))]
-    partial class ReplayContextModelSnapshot : ModelSnapshot
+    [Migration("20240824080519_UpdateBroodlings")]
+    partial class UpdateBroodlings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
