1808db57461dce399cd73b05e1b5514e268e6574
ipax77 <ph.ilipp@web.de>
2025-07-18 09:27:33 +0200

    add group synergy

--- src/dsstats.shared/Data.cs
+++ src/dsstats.shared/Data.cs
@@ -1333,7 +1333,7 @@ public static class Data
 
     public const string ReplayBlobDir = "/data/ds/replayblobs";
     public const string MysqlFilesDir = "/data/mysqlfiles";
-    public const string WasmVersion = "2.0.8";
+    public const string WasmVersion = "2.0.9";
 }
 
 public class LatestReplayEventArgs : EventArgs
