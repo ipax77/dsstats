48ed83f622fceb766cf66a3a4f54114b202f2582
ipax77 <ph.ilipp@web.de>
2026-01-05 11:34:23 +0100

    cu

--- src/server/dsstats.api/Program.cs
+++ src/server/dsstats.api/Program.cs
@@ -105,8 +105,8 @@ using var scope = app.Services.CreateScope();
 using var dbContext = scope.ServiceProvider.GetRequiredService<DsstatsContext>();
 dbContext.Database.Migrate();
 
-var transitionService = scope.ServiceProvider.GetRequiredService<TransitionService>();
-transitionService.FixHashes().Wait();
+//var transitionService = scope.ServiceProvider.GetRequiredService<TransitionService>();
+//transitionService.FixHashes().Wait();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
